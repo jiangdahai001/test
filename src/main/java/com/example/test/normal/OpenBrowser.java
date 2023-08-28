1c4f478c17caf8b0473111081d8cc2bb72a01df0
jiangdahai <jiangdh@geneplus.org.cn>
2023-08-28 16:14:27 +0800

    chrome的APP和演示模式

--- src/main/java/com/example/test/normal/OpenBrowser.java
+++ src/main/java/com/example/test/normal/OpenBrowser.java
@@ -2,12 +2,29 @@ package com.example.test.normal;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.net.URI;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.prefs.Preferences;
 
 public class OpenBrowser {
   public static void main(String[] args) {
+    exec();
+  }
+  public static void exec() {
+    String cmd = "\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" --kiosk geneplus.cn";
+//    String cmd = "\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" --app=https://geneplus.cn --start-fullscreen";
+//    String cmd = "\"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe\" --app=https://geneplus.cn --start-fullscreen";
+//    String cmd = "\"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe\" --kiosk https://geneplus.cn --edge-kiosk-type=fullscreen";
+//    String cmd = "\"C:\\Program Files\\Mozilla Firefox\\firefox.exe\" --kiosk https://geneplus.cn";
+    try {
+      Runtime.getRuntime().exec(cmd);
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+  public static void browser() {
     try {
       // 创建一个URI对象，指定要打开的URL
       URI uri = new URI("https://geneplus.cn");
